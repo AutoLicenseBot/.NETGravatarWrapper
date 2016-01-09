75db4a270d21921646fde81a5de15d46f07b4920
AutoLicenseBot <AutoLicenseBot@gmail.com>
2016-01-09 10:38:43 -0500

    Add License

---
+++ GravatarWrapper/Exceptions/NoResponseReturnedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GravatarWrapper.Exceptions
+{
+    class NoResponseFoundException : Exception
+    {
+        public NoResponseFoundException()
+            : base("No response returned from server"){}
+    }
+}
